a6fb0044453857f18d0262e37489be795280081d
Krzysztof Sobczak <contact@krzysztofsobczak.com>
2015-12-03 00:07:59 +0100

    comments, clean

--- lab3/zloty_podzial.m
+++ lab3/zloty_podzial.m
@@ -1,19 +1,22 @@
-function x_min = ZP(f, a, b, epsilon)
-                 
+function x_min = zloty_podzial(f, a, b, epsilon)
+
+iter= 50;                       
 c=((sqrt(5)-1)/2);      
 k=0;                    
 
+% równowa?ne x1=b-c(b-a)
 x1=a+(1-c)*(b-a);       
 x2=a+c*(b-a);
 
 f_x1=f(x1);             
 f_x2=f(x2);
 
-while ((abs(b-a)>epsilon))
+while ((abs(b-a)>epsilon) && (k<iter))
     k=k+1;
     if(f_x1<f_x2)
         b=x2;
         x2=x1;
+        % równowa?ne x1=b-c(b-a)
         x1=a+(1-c)*(b-a);
         
         f_x1=f(x1);
@@ -28,14 +31,15 @@ while ((abs(b-a)>epsilon))
         f_x2=f(x2);
 
     end
-    a
-    b
+    
     k=k+1;
 end
 
-k
+
 if(f_x1<f_x2)
-    x_min = x1;
+    sprintf('x_min=%f', x1)
+    sprintf('f(x_min)=%f ', f_x1)
 else
-	x_min = x2;
+    sprintf('x_min=%f', x2)
+    sprintf('f(x_min)=%f ', f_x2)
 end
